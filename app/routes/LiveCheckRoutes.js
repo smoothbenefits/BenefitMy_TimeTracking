9e036ff29285e300a4992d40acbbd97a1b9b1046
zhangsiy <jeffzhang_misc@hotmail.com>
2016-02-03 23:28:51 -0500

    BM-770 initial setup of email service

--- app/routes/LiveCheckRoutes.js
+++ app/routes/LiveCheckRoutes.js
@@ -1,9 +1,27 @@
 module.exports = function(app) {
 
+    // TODO: Cleat this testing code once it is ready
+    var sendTestEmail = function() {
+        var emailService = require('../services/EmailService')
+
+        var contextData = {
+            company: { name: "Alibaba Ltd" }
+        };
+
+        emailService.sendSupportEmailWithTemplate(
+            ['zhangsiy@hotmail.com'],
+            'OMG3',
+            'timeoff_request',
+            contextData
+        );
+    };
+
     app.get('/livecheck', function(req, res) {
+
+        // TODO: Cleat this testing code once it is ready
+        sendTestEmail();
 
         res.setHeader('Cache-Control', 'no-cache');
         res.json({ 'message': 'Service is Live!' });
-
     });
 };
---
+++ app/settings/appSettings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+    emailConfig : {
+        emailHost: 'smtp.sendgrid.net',
+        emailHostUser: 'frank.qiu@gmail.com',
+        emailHostPassword: 'BenefitMy2015',
+        emailPort: 587,
+        emailUseTls: true,
+
+        // TODO: Put in the real support email once the feature is 
+        //       ready
+        appSupportEmailAddress: 'jeff.zhang.82@gmail.com'
+    },
+
+    mainAppSiteUrl : 'http://app.workbenefits.me/'
+};
