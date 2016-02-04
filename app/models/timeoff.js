4c330b8c614d89fc8b7bb1ab1b499162172dc0ee
Zimeng Chen <eagleofpc@gmail.com>
2016-02-03 20:41:13 -0500

    Take in review feedback

--- app/models/timeoff.js
+++ app/models/timeoff.js
@@ -6,13 +6,20 @@ module.exports = mongoose.model('Timeoff', {
     duration: Number,
     status: String,
     type: String,
-    requestor: {  email : String,
+    requestor: {  email : { 
+            type: String, 
+            required: true 
+        },
         firstName: String,
         lastName: String,
-        foreignKey: String},
-    approver: { email : String,
+        personDescriptor: String},
+    approver: { email : { 
+            type: String, 
+            required: true 
+        },
         firstName: String,
         lastName: String,
-        foreignKey: String },
-    timestamp: { type: Date, default: Date.now}
+        personDescriptor: String },
+    requestTimestamp: { type: Date, default: Date.now},
+    decisionTimestamp: { type: Date }
 });
