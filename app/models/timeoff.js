e770d74e119fafc78640abf356be60c2d24d40ec
Zimeng Chen <eagleofpc@gmail.com>
2016-02-03 21:04:57 -0500

    Make required fields required

--- app/models/timeoff.js
+++ app/models/timeoff.js
@@ -2,24 +2,33 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 module.exports = mongoose.model('Timeoff', {
-    startDateTime: {type: Date, default: Date.now},
-    duration: Number,
-    status: String,
-    type: String,
-    requestor: {  email : { 
+    startDateTime: { type: Date, required: true },
+    duration: { type: Number, required: true },
+    status: { type: String, required: true },
+    type: { type: String, required: true },
+    requestor: {  
+        email : { 
             type: String, 
             required: true 
         },
         firstName: String,
         lastName: String,
-        personDescriptor: String},
-    approver: { email : { 
+        personDescriptor: { 
+            type: String, 
+            required: true
+        }
+    },
+    approver: { 
+        email : { 
             type: String, 
             required: true 
         },
         firstName: String,
         lastName: String,
-        personDescriptor: String },
-    requestTimestamp: { type: Date, default: Date.now},
+        personDescriptor: { 
+            type: String, 
+            required: true}
+        },
+    requestTimestamp: { type: Date, default: Date.now },
     decisionTimestamp: { type: Date }
 });
