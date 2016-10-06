b16f990c3d69b2f837c89314df3753d032d50b6e
Xiaoyu Lei <thunder.rain.a@gmail.com>
2016-10-05 21:38:42 -0400

    Poll queue for messages

--- config/server.js
+++ config/server.js
@@ -1,4 +1,10 @@
 module.exports = {
 	port : '6999',
-	google_api_key: 'AIzaSyBpkYJswxepCkGsVUu2gAcYcEZ6vXI1cY8'
+	google_api_key: 'AIzaSyBpkYJswxepCkGsVUu2gAcYcEZ6vXI1cY8',
+	aws: {
+		region: 'us-west-2',
+		access_id: 'AKIAJKK4SB77LNWSFRKA',
+		secret_key: '',
+		time_accural_queue_url: "https://sqs.us-west-2.amazonaws.com/726981589053/dev-time-accural-queue"
+	}
 };
